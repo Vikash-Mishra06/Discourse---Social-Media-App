95db7b8cc4fa22491c406216d701d3ef3c13fc48
Vikash Mishra <mishravikash0612@gmail.com>
2025-10-25 20:27:32 +0530

    `Added navigation functionality to PostCard and Messages components`

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import { BadgeCheck, Heart, MessageCircle, Share2 } from 'lucide-react'
 import moment from 'moment'
 import { dummyUserData } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const PostCard = ({ post }) => {
+    const navigate = useNavigate()
     const [likes, setLikes] = useState(post.likes_count)
     const currentUser = dummyUserData
 
@@ -13,7 +15,7 @@ const PostCard = ({ post }) => {
 
     return (
         <div className='bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl'>
-            <div className='inline-flex items-center gap-3 cursor-pointer'>
+            <div onClick={() => navigate('/profile/' + post.user._id)} className='inline-flex items-center gap-3 cursor-pointer'>
                 <img src={post.user.profile_picture} alt="" className='w-10 h-10 rounded-full shadow' />
                 <div>
                     <div className='flex items-center space-x-1'>
