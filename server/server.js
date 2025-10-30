54ab11677f1b96abe7ceb689c05a985acd4335f1
Vikash Mishra <mishravikash0612@gmail.com>
2025-10-30 17:57:18 +0530

    Integrated server folder added express and mongodb also Inngest

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/db.js'
+import {inngest, functions} from './inngest/index.js'
+import { serve } from "inngest/express";
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+await connectDB()
+
+app.get('/', (req, res) => {
+    res.send('Hello from server')
+})
+
+app.use('/api/inngest', serve({ client: inngest, functions }));
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
