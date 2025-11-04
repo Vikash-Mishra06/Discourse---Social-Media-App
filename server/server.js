5d183b540f86e7672eab256e50de6329b65d05af
Vikash Mishra <mishravikash0612@gmail.com>
2025-11-04 14:42:15 +0530

    "Added dependencies and modified server.js to use inngest"

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
-import connectDb from './db/db.js'
-connectDb()
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDb from "./db/db.js";
+import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
 
-const app = express()
+connectDb();
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => res.send("Server is running"))
+app.get("/", (req, res) => res.send("Server is running"));
+app.use('/api/inngest', serve({ client: inngest, functions }));
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,() => console.log(`Server is running on port: ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
